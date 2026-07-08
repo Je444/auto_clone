07664e785e898b5a18808096add42a8f618444f1
Je444 <tyqw123rty@gmail.com>
2026-07-08 08:28:44 +0000

    全英文建議

---
+++ NTUB/day1/14.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a)>0 and (b)>0 and (c)>0:
+    print("True")
+else:
+    print("False")
+
+if (a)==0 or (b)==0 or (c)==0:
+    print("True")
+else:
+    print("False")
