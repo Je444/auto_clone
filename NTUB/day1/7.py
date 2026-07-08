8858cf4ae3a59495c5aee6dcd37c0c86800a413a
Je444 <tyqw123rty@gmail.com>
2026-07-08 06:20:06 +0000

    全英文建議

--- NTUB/day1/7.py
+++ NTUB/day1/7.py
@@ -2,5 +2,5 @@ now_money=int(input())
 b=float(input())
 c=int(input())
 for rs in range(c):
-    now_money=now_money*(111+b/1200)
+    now_money=now_money*(1+b/1200)
     print(f"{rs+1},{now_money:.2f}")
